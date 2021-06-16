a935901a68a6ff0592585aecbec92ff9b83b624b
Skills <skills@Repairs-MacBook-Pro.local>
2021-06-16 22:25:16 +0300

    final fixes

--- src/services/soccerService.js
+++ src/services/soccerService.js
@@ -9,41 +9,49 @@ export const soccerService = {
 };
 
 async function queryLeagues() {
-    const leagues = [2000, 2001, 2002, 2003, 2013].map(id => {
-        const league = axios({
-            method: 'GET',
-            url: `${BASE_URL}/${id}`,
-            headers: { 'X-Auth-Token': API_TOKEN },
+    try {
+        const leagues = [2000, 2001, 2002, 2003, 2013].map(id => {
+            const league = axios({
+                method: 'GET',
+                url: `${BASE_URL}/${id}`,
+                headers: { 'X-Auth-Token': API_TOKEN },
 
+            });
+            return league
+        })
+
+        const leaguesDetails = await Promise.all(leagues)
+        return leaguesDetails.map((league) => {
+
+            const { id, name } = league.data;
+
+            return {
+                id,
+                name
+            };
         });
-        return league
-    })
-
-    const leaguesDetails = await Promise.all(leagues)
-    return leaguesDetails.map((league) => {
-
-        const { id, name } = league.data;
-
-        return {
-            id,
-            name
-        };
-    });
+    } catch (err) {
+        throw new Error('Failed to fetch leagues', err)
+    }
 }
 
 async function queryTeams(id) {
-    const data = await axios({
-        method: 'GET',
-        url: `${BASE_URL}/${id}/teams`,
-        headers: { 'X-Auth-Token': API_TOKEN },
+    try {
+        const data = await axios({
+            method: 'GET',
+            url: `${BASE_URL}/${id}/teams`,
+            headers: { 'X-Auth-Token': API_TOKEN },
 
-    });
-    return data.data.teams.map(team => {
-        const { id, name, crestUrl } = team
-        return {
-            id,
-            name,
-            crestUrl
-        }
-    });
+        });
+        return data.data.teams.map(team => {
+            const { id, name, crestUrl } = team
+            return {
+                id,
+                name,
+                crestUrl
+            }
+        });
+    } catch (err) {
+        throw new Error('Failed to fetch teams', err)
+    }
 }
