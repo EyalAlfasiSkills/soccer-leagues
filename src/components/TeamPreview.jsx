600265e882c48f49e5e1cf10f114acc75718737d
Skills <skills@Repairs-MacBook-Pro.local>
2021-06-16 21:48:04 +0300

    finished teams list

--- src/components/TeamPreview.jsx
+++ src/components/TeamPreview.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export const TeamPreview = ({ team }) => {
+    console.log(team);
     return (
         <div className="team-preview-wrapper">
-
+            <div className="crest-container ">
+                <img
+                    src={team.crestUrl ? team.crestUrl : 'https://image.flaticon.com/icons/png/512/189/189665.png'}
+                    alt="team crest image"
+                />
+            </div>
+            <div className="content">
+                <h3 className="team-name">{team.name}</h3>
+            </div>
         </div>
     )
 }
